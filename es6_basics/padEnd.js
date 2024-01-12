909968c8c066dcd1076424f1fbd585679332c605
arkoaikins <arkoaikins@gmail.com>
2024-01-12 17:15:59 -0500

    add padStart and padEnd js files

---
+++ es6_basics/padEnd.js
@@ -0,0 +1,5 @@
+let example = 'kofi';
+console.log(example.padEnd(10,'a'));
+
+//so it makes sure it is 10 character,if not it adds a to it
+// to make the 10 characters
---
+++ es6_basics/padStart.js
@@ -0,0 +1,4 @@
+let example = 'Kofi'
+
+console.log(example.padStart(10, 'a'))
+//so in all then it gives 10 characters
