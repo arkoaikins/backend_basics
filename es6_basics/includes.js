855b72d443f8be062083161f1f6fd0ee0ece4bf7
arkoaikins <arkoaikins@gmail.com>
2024-01-12 17:32:46 -0500

    add include file

---
+++ es6_basics/includes.js
@@ -0,0 +1,12 @@
+//the include keyword in js
+let numArray = [1,2,3,4,5]
+
+console.log(numArray.includes(7))
+
+// and also the push keyword add a value to an array
+numArray.push(7)
+console.log(numArray.includes(7))
+//so you could see that it is not true
+
+// so [] is for arrays
+// and {} is for objects in JS  
