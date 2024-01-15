02098a1a65d067be3463e72291a86a5445bb914a
arkoaikins <arkoaikins@gmail.com>
2024-01-14 20:35:59 -0500

    add how to write class in js

---
+++ es6_classes/defClass.js
@@ -0,0 +1,10 @@
+// classes a way to make javascript more object oriented way
+//so you define a class this way
+class Animal {
+    constructor(head, tail) { //we use the constructor to write the instances of the class(Animal) so meaning the animal has a head and tail
+        this.head = head
+        this.tail = tail
+
+    }
+
+}
