9e883ff23fdab646d936de4503e1dd962a74858f
arkoaikins <arkoaikins@gmail.com>
2024-01-15 15:58:36 -0500

    imports from defClass

---
+++ es6_classes/import.js
@@ -0,0 +1,9 @@
+import { Animal } from './defClass.js';
+let cat = new Animal();
+console.log(cat);
+//run it `npm run dev import.js`
+//so now we just made cat as an object of the class animal and passed nothing to it
+//so u get "Animal(head:undefined, tail:undefined"
+
+
+//now move to `addParam.js`
