d592340e8ff43498bc39b68f0b6d1a20441ad9df
arkoaikins <arkoaikins@gmail.com>
2024-01-15 19:32:07 -0500

    import from statMet.js

---
+++ es6_classes/importstatMet.js
@@ -0,0 +1,11 @@
+import { Animal } from './statMet.js'
+
+let dog = new Animal('big', 'long')
+console.log(dog.head)
+
+//now let me use the normal function
+dog.makenoise('big sound', 'long sound')
+
+// now let's look at the static method which you do not need
+// the dog instance,you can use it straight away because it is wit the class itself
+console.log(Animal.cry())
