e3836bb67c835d2d6b7aa5b7e59eb4b519dc6447
arkoaikins <arkoaikins@gmail.com>
2024-01-16 17:07:01 -0500

    add importing method.js file

---
+++ es6_classes/importgetmethod.js
@@ -0,0 +1,9 @@
+import { Human, Circle } from './getmethod.js'
+
+let man = new Human('book', 'james');
+console.log(man.features);
+
+const myCircle = new Circle(5);
+
+console.log(myCircle.diameter);
+console.log(myCircle.area);
