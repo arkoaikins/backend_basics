a4337546ad8da5e67185f3671ddbca57aea0d3a8
arkoaikins <arkoaikins@gmail.com>
2024-01-26 20:51:47 -0500

    [manipulation]:how to update map in js

---
+++ es6_dataManipulation/updatekey_value.js
@@ -0,0 +1,19 @@
+// So how are you going to update a value in Map
+
+//Creating a Map with some initial key-value pairs
+const map = new Map([
+    ['key1', 50],
+    ['key2', 75],
+    ['key3', 125]
+]);
+
+for(let [key, value] of map) {
+    console.log(key, value);
+} 
+
+// To update the value of 'key2'
+map.set('key2', 100);
+
+//check if the update was sucessful
+console.log(map.get('key2')); //so this will not output the value 100
+
